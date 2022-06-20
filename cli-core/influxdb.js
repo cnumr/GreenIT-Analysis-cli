8f76ed05c1c17adaa50abce087cacceed8521c22
Damien-Ar <damien.aranda42@gmail.com>
2022-06-20 15:17:47 +0200

    Mise au prope du code

--- cli-core/influxdb.js
+++ cli-core/influxdb.js
@@ -1,25 +1,24 @@
-const {InfluxDB, Point, HttpError} = require('@influxdata/influxdb-client')
+const {InfluxDB, Point, HttpError} = require('@influxdata/influxdb-client');
 const fs = require('fs');
 const ProgressBar = require('progress');
 
 async function write(reports, options) {
 
     if (!options.influxdb_hostname) {
-        throw `You must define an InfluxDB hostname.`
+        throw `You must define an InfluxDB hostname.`;
     }
     if (!options.influxdb_token) {
-        throw `You must define an InfluxDB token.`
+        throw `You must define an InfluxDB token.`;
     }
     if (!options.influxdb_bucket) {
-        throw `You must define an InfluxDB bucket name.`
+        throw `You must define an InfluxDB bucket name.`;
     }
     if (!options.influxdb_org) {
-        throw `You must define an InfluxDB organisation.`
+        throw `You must define an InfluxDB organisation.`;
     }
 
     const url = options.influxdb_hostname;
-    //const org = 'ecoindex'
-    //const bucket = options.influxdb_database;
+
     //initialise progress bar
     let progressBar;
     if (!options.ci){
@@ -29,22 +28,19 @@ async function write(reports, options) {
             width: 40,
             total: reports.length+2
         });
-        progressBar.tick()
+        progressBar.tick();
     } else {
         console.log('Push report to InfluxDB ...');
     }
 
-    
-    /*const writeApi = new InfluxDB({url, timeout: 150, token: `$D0eC0bDHkaxbBrCCjqQ-dSno4dVrA9zH7kBskya3CXSd3UUUcHCoPY4_7awIpvrgwV7KJUTA6o0-uGtzOG32JQ==`})
-        .getWriteApi(org, bucket, 'ms')
-    writeApi.useDefaultTags({location: url})*/
+    // initialise client
+    const client = new InfluxDB({url: options.influxdb_hostname, token: options.influxdb_token});
+    const writeApi = client.getWriteApi(options.influxdb_org, options.influxdb_bucket);
 
-    const client = new InfluxDB({url: options.influxdb_hostname, token: options.influxdb_token})
-    const writeApi = client.getWriteApi(options.influxdb_org, options.influxdb_bucket)
-
-    reports.forEach((file) => {
+    // create points from reports
+    const points = reports.map((file) => {
         let obj = JSON.parse(fs.readFileSync(file.path).toString());
-        let hostname = obj.url.split('/')[2]
+        let hostname = obj.url.split('/')[2];
         let point = new Point("eco_index")
             .tag("pageName", obj.pageInformations.name)
             .tag("hostname", hostname)
@@ -63,26 +59,31 @@ async function write(reports, options) {
             .floatField("emptySrcTagNumber", obj.emptySrcTagNumber)
             .floatField("inlineJsScriptsNumber", obj.inlineJsScriptsNumber)
             .floatField("responsesSize", Math.round(obj.responsesSize / 1000))
-            .floatField("responsesSizeUncompress", Math.round(obj.responsesSizeUncompress / 1000))
-        Object.keys(obj.bestPractices).map(key => point.stringField(key, obj.bestPractices[key].complianceLevel || 'A'))
+            .floatField("responsesSizeUncompress", Math.round(obj.responsesSizeUncompress / 1000));
 
-        writeApi.writePoint(point)
+        Object.keys(obj.bestPractices)
+            .map(key => point.stringField(key, obj.bestPractices[key].complianceLevel || 'A'));
         
         if (progressBar) progressBar.tick()
-    })
+
+        return point
+    });
+
+    //upload points and close connexion
+    writeApi.writePoints(points);
 
     writeApi
         .close()
         .then(() => {
-            if (progressBar) progressBar.tick()
+            if (progressBar) progressBar.tick();
         })
         .catch(e => {
-            console.log('Writing to influx failed\n')
-            console.error(e)
+            console.log('Writing to influx failed\n');
+            console.error(e);
             if (e instanceof HttpError && e.statusCode === 401) {
-                console.log(`The InfluxDB database: ${bucket} doesn't exist.`)
+                console.log(`The InfluxDB database: ${bucket} doesn't exist.`);
             }
-        })
+        });
 }
 
 module.exports = {
